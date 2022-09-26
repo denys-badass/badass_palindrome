6070d78d268d99aab11861aa638dc9b208310c8e
Denys Podobrii <denys.podobrii@gmail.com>
2022-09-26 21:07:38 +0300

    Add Integer palidrome method

--- lib/badass_palindrome.rb
+++ lib/badass_palindrome.rb
@@ -2,7 +2,9 @@
 
 require_relative "badass_palindrome/version"
 
-class String
+module BadassPalindrome
+  
+
 
   def palindrome?
     down_text == down_text.reverse
@@ -11,7 +13,15 @@ class String
   private
 
   def down_text
-    scan(/[a-z]/i).join.downcase
+    to_s.scan(/[a-z0-9]/i).join.downcase
   end
 
+end
+
+class String
+  include BadassPalindrome
+end
+
+class Integer 
+  include BadassPalindrome
 end
